8c49195d046ed1069e61920a6618ddfd2c114ccb
Aarni Halinen <aarni.halinen@aalto.fi>
2019-02-09 14:12:10 +0200

    Use material-ui sliders. Rename own Slider component to InputSlider

--- src/components/InputContainer/InputContainer.js
+++ src/components/InputContainer/InputContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Slider from '../Slider/Slider';
-import InputRange from 'react-input-range';
+import InputSlider from '../InputSlider/InputSlider';
 
 export default class InputContainer extends Component {
   constructor(props) {
@@ -9,52 +8,31 @@ export default class InputContainer extends Component {
     }
   }
 
-  handleIncome = () => {
-
-  }
-
-  handleTax = (value) => {
-    // this.setState({
-    //   value,
-    // });
-    // this.props.tax(value);
-  }
-
-  handleYear = () => {
-
-  }
-
   render() {
     return (
       <div>
-        <Slider id="income" label="Income"
+        <InputSlider id="income" label="Income"
           min={100}
           max={10000}
           defaultVal={100}
-          handlerFunc={this.props.income} />
-        <Slider id="tax" label="Tax percentage"
+          handlerFunc={this.props.income}
+          step={100}
+        />
+        <InputSlider id="tax" label="Tax percentage"
           min={0}
           max={100}
           defaultVal={5}
-          handlerFunc={this.props.tax} />
-        <Slider id="year" label="Income"
+          handlerFunc={this.props.tax}
+          step={1}
+        />
+        <InputSlider id="year" label="Income"
           min={2014}
           max={2018}
           defaultVal={2014}
-          handlerFunc={this.props.year} />
+          handlerFunc={this.props.year}
+          step={1}
+        />
       </div>
-      // <InputRange
-      //   maxValue={100}
-      //   minValue={0}
-      //   value={this.state.tax}
-      //   onChange={tax => this.setState({tax})}
-      // />
-      // <InputRange
-      // maxValue={20}
-      // minValue={0}
-      // value={this.state.value}
-      // onChange={value => this.setState({ value })}
-      // onChangeComplete={value => console.log(value)} />
     )
   }
 }
