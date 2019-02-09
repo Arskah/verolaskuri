1f4da1383dae669573a73b96e76592f19e8e2814
Rikkokiri <pilvi.rajala@gmail.com>
2019-02-09 13:43:25 +0200

    Data is correctly imported with Python. Extra columns are dropped.

--- dataprocessing/csvtojson.py
+++ dataprocessing/csvtojson.py
@@ -1,8 +1,30 @@
 import pandas as pd
 
-budgetFilename = 'Talousarvio-fi.csv'
 
-columnnames = ['Vuosi', 'Paaluokka/Osasto', 'Luku', 'Momentti', 'Alkup.talousarvio', 'Lisätalousarvio',	'Voimassaoleva talousarvio', 'Käytettävissä', 'Nettokertymä ko.vuodelta', 'Nettokertymä','Jäljellä']
+budgetFilename = 'Talousarvio-modified.csv'
+columnnames = ['Vuosi', 'Paaluokka', 'Luku', 'Momentti', 'Alkup.talousarvio', 'Lisatalousarvio', 'Voimassaoleva talousarvio', 'Kaytettavissa', 'Nettokertyma vuodelta', 'Nettokertyma','Jaljella']
 
-rawdata = pd.read_csv(budgetFilename, delimiter='\t', names=columnnames)
-print(rawdata)
+# Place label names to variables for easier usage
+yearLabel = 'Vuosi'
+categoryLabel = 'Paaluokka/Osasto'
+subcategoryLabel = 'Momentti'
+budgetLabel = 'Voimassaoleva talousarvio'
+
+# INPUT: Important datafields:
+# Vuosi
+# Paaluokka/Osasto
+# Momentti (subcategory)
+# Voimassaoleva talousarvio
+
+# ================================================================================
+
+# Import the data
+rawdata = pd.read_csv(budgetFilename, sep='\t', names=columnnames, index_col=False)
+#print(rawdata)
+
+todrop = ['Luku', 'Alkup.talousarvio', 'Lisatalousarvio', 'Kaytettavissa', 'Nettokertyma', 'Nettokertyma vuodelta', 'Jaljella']
+wantedData = rawdata.drop(labels=todrop, axis=1)
+print(wantedData)
+
+print(list(rawdata.columns.values))
+
