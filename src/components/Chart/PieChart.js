c420d1da6168d2ccf09fd5bdd69c626d65d01654
Tuomas Taubert <tuomas.taubert@hotmail.com>
2019-02-09 16:54:00 +0200

    PieChart labels finally more or less working

--- src/components/Chart/PieChart.js
+++ src/components/Chart/PieChart.js
@@ -3,7 +3,9 @@ import * as d3 from 'd3';
 
 const height = 400;
 const width = 400;
+const radius = Math.min(width, height) / 2;
 var values = [1,2,3,4];
+var labels = ["Sos. ter. ministeriö","Sisäministeriö"];
 var pie = d3.pie()(values);
 
 export default class PieChart extends Component {
@@ -11,8 +13,11 @@ export default class PieChart extends Component {
   constructor(props){
       super(props);
       values = [];
-      for(var i=0; i<this.props.data.length; i++)
+      labels = [];
+      for(var i=0; i<this.props.data.length; i++){
         values[i] = this.props.data[i].percentage;
+        labels[i] = this.props.data[i].name;
+      }
       pie = d3.pie()(values);
   }
   render() {
@@ -22,13 +27,19 @@ export default class PieChart extends Component {
         <svg height={height} width={width}>
             <g transform={'translate('+(width/2)+','+(height/2)+')'}>
                 <Slice pie={pie} />
+                <Labels pie={pie} labels={labels} />
             </g>
+            
         </svg>
       </div>
     )
   }
   
 }
+
+function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
 
 const Slice = props => {
   let { pie } = props;
@@ -44,5 +55,31 @@ const Slice = props => {
     let sliceColor = interpolate(index / (pie.length - 1));
 
     return <path d={arc(slice)} fill={sliceColor} />;
+  });
+};
+const Labels = props => {
+  let { pie } = props;
+  let { labels } = props;
+
+  let arc = d3
+    .arc()
+    .innerRadius(140)
+    .outerRadius(140);
+    
+  return pie.map((slice, index) => {
+    let angle1 = slice.startAngle;
+    let angle2 = slice.endAngle;
+    let angle3 = (angle1 + angle2) / 2;
+    let nx = (radius*1.8) * Math.cos(angle3);
+    let ny = (radius*1.8) * Math.sin(angle3);
+    let textAnchoring = "start";
+    if(nx < 0)
+        textAnchoring = "end";
+        
+    
+    //pos[0] = radius * (Math.PI *2 - d2);
+	/*pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+    pos[0] = radius;*/
+    return <text textAnchor={textAnchoring} x={nx} y="0">{labels[index]}</text>;
   });
 };
