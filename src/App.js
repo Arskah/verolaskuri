693596c3788091979492007acadc3813df7fc70c
Aarni Halinen <aarni.halinen@aalto.fi>
2019-02-09 17:25:39 +0200

    Clean un-used imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import InputContainer from './components/InputContainer/InputContainer';
 
-import Chart from './components/Chart/Chart';
 import PieChart from './components/Chart/PieChart';
 
 let testArray = [{"percentage":5, "sum" : 100, "name":"Test1"},{"percentage":10, "sum" : 100, "name":"Test2"},{"percentage":15, "sum" : 100, "name":"Test3"},{"percentage":20, "sum" : 100, "name":"Test4"},{"percentage":25, "sum" : 100, "name":"Test5"},{"percentage":30, "sum" : 100, "name":"Test6"},{"percentage":35, "sum" : 200, "name":"Test7"}];
